2358bdb0503daa4c9baaf57c0e085ce482dee197
XxbazingaxX <bazingasteam@gmail.com>
2023-07-10 14:00:54 +0200

    Add files via upload

---
+++ Tema3.java
@@ -0,0 +1,14 @@
+public class Tema3{
+
+    public static void main(String[] args){
+
+        String[] nombres = new String[]{"Carlos", "Juan", "Antonio", "Angela"};
+
+        for (String nombre : nombres) {
+
+            System.out.print(nombre + " ");
+        }
+
+    }
+
+}
