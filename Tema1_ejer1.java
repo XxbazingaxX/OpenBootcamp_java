60342f06d5f02d315487077e549626f478acf3dd
XxbazingaxX <bazingasteam@gmail.com>
2023-07-10 13:06:47 +0200

    Add files via upload

---
+++ Tema1_ejer1.java
@@ -0,0 +1,29 @@
+
+
+public class Tema1 {
+
+    /**
+    * Ejemplos de tipo de datos
+    * @param args - argumentos de entrada para la aplicación
+    */
+        public static void main(String[] args) {
+            
+            //numericos
+            int numero = 14;
+            long a = 100;
+            double b = 5.5;
+
+
+            //booleano
+            boolean c = false;
+
+            //texto
+            String texto = "Hola mundo";
+
+            System.out.println(numero);
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println(texto);
+    }
+}
